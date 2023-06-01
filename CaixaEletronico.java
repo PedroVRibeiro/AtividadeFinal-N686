24d72b2dd342dc769a9633b495951fdc81e5df34
luizreboucas <primoneto@gmail.com>
2023-06-01 10:42:36 -0300

    classe Caixa Eletrônico implementada - Luiz Rebouças

---
+++ CaixaEletronico.java
@@ -0,0 +1,18 @@
+public class CaixaEletronico {
+
+    private int total;
+
+    public CaixaEletronico() {
+        this.total = 1000;
+    }
+
+    public int sacar(int valor) {
+        if (valor > total) {
+            System.out.println("Não há dinheiro suficiente, tente outro caixa");
+        } else {
+            total -= valor;
+            return valor;
+        }
+        return valor;
+    }
+}
